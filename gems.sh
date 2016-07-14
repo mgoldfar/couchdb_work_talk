446acbf6b31198a38b51313ddaf79770ffed3fc8
Michael Goldfarb <michael.goldfarb@gmail.com>
2016-07-13 21:55:40 -0700

    Adding ruby import script and script to download the data source.

---
+++ gems.sh
@@ -0,0 +1,2 @@
+gem install libxml-ruby -- --with-xml2-include=/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.11.sdk/usr/include/libxml2 --use-system-libraries
+gem install couchrest
---
+++ get_data.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+curl http://img.jamendo.com/data/dbdump_artistalbumtrack.xml.gz > dbdump_artistalbumtrack.xml.gz
+  
